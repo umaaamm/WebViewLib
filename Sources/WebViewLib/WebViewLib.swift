135eb7e5c699ecabb68cd67811472156abf94ece
umaaamm <umam.tekno@gmail.com>
2023-04-06 10:55:19 +0700

    initial commit

--- Sources/WebViewLib/WebViewLib.swift
+++ Sources/WebViewLib/WebViewLib.swift
@@ -1,6 +1,35 @@
-public struct WebViewLib {
-    public private(set) var text = "Hello, World!"
+//public struct WebViewLib {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
 
+
+import Foundation
+import UIKit
+import WebKit
+
+public class WebViewLib: WKWebView {
+    
     public init() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true
+        super.init(frame: .zero, configuration: webConfiguration)
+        self.scrollView.isScrollEnabled = false
+        self.isMultipleTouchEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    @discardableResult
+    public func load() -> WKNavigation? {
+        if let url = URL(string:"http://google.com") {
+            let req = URLRequest(url: url)
+            return super.load(req)
+        }
+        return nil
     }
 }
--- Tests/WebViewLibTests/WebViewLibTests.swift
+++ Tests/WebViewLibTests/WebViewLibTests.swift
@@ -6,6 +6,6 @@ final class WebViewLibTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WebViewLib().text, "Hello, World!")
+//        XCTAssertEqual(WebViewLib().text, "Hello, World!")
     }
 }
